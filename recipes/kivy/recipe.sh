078176b295a5f441e9e048d7dc356e8e5faa9b87
neymarjohn <seriousman1994217winter@gmail.com>
2012-11-14 21:47:54 +0100

    Get Android network state by direct query or with intent

--- recipes/kivy/recipe.sh
+++ recipes/kivy/recipe.sh
@@ -2,7 +2,7 @@
 
 VERSION_kivy=
 URL_kivy=https://github.com/kivy/kivy/zipball/master/kivy-master.zip
-DEPS_kivy=(pygame pyjnius android)
+DEPS_kivy=(pygame android)
 MD5_kivy=
 BUILD_kivy=$BUILD_PATH/kivy/$(get_directory $URL_kivy)
 RECIPE_kivy=$RECIPES_PATH/kivy
