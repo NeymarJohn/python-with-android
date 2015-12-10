bd93f6d83946a4ceb757966310671bbc58ccf316
neymarjohn <seriousman1994217winter@gmail.com>
2015-12-10 01:23:55 +0100

    biglink: ensure that no empty argument will be used

--- pythonforandroid/recipes/pyopenssl/__init__.py
+++ pythonforandroid/recipes/pyopenssl/__init__.py
@@ -22,7 +22,7 @@ class PyOpenSSLRecipe(CompiledComponentsPythonRecipe):
         if exists(join(build_dir, '.patched')):
             print('pyOpenSSL already patched, skipping')
             return
-        self.apply_patch('fix-dlfcn.patch', arch.arch)
+        self.apply_patch('fix-dlfcn.patch')
         shprint(sh.touch, join(build_dir, '.patched'))
 
     def get_recipe_env(self, arch):
