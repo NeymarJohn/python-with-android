80a46f53689a382ef21d566f37172b6c23502cc1
neymarjohn <seriousman1994217winter@gmail.com>
2016-01-12 10:31:03 -0600

    fix root extraction from zip

--- pythonforandroid/recipe.py
+++ pythonforandroid/recipe.py
@@ -2,7 +2,7 @@ from os.path import join, dirname, isdir, exists, isfile, split, realpath
 import importlib
 import zipfile
 import glob
-from six import PY2, with_metaclass
+from six import PY2
 
 import sh
 import shutil
@@ -37,19 +37,8 @@ else:
             return SourceFileLoader(module, filename).load_module()
 
 
-class RecipeMeta(type):
-    def __new__(cls, name, bases, dct):
-        if name != 'Recipe':
-            if 'url' in dct:
-                dct['_url'] = dct.pop('url')
-            if 'version' in dct:
-                dct['_version'] = dct.pop('version')
-
-        return super(RecipeMeta, cls).__new__(cls, name, bases, dct)
-
-
-class Recipe(with_metaclass(RecipeMeta)):
-    _url = None
+class Recipe(object):
+    url = None
     '''The address from which the recipe may be downloaded. This is not
     essential, it may be omitted if the source is available some other
     way, such as via the :class:`IncludedFilesBehaviour` mixin.
@@ -63,7 +52,7 @@ class Recipe(with_metaclass(RecipeMeta)):
               if you want.
     '''
 
-    _version = None
+    version = None
     '''A string giving the version of the software the recipe describes,
     e.g. ``2.0.3`` or ``master``.'''
 
@@ -98,16 +87,6 @@ class Recipe(with_metaclass(RecipeMeta)):
     at build time, you must create a recipe.'''
 
     archs = ['armeabi']  # Not currently implemented properly
-
-    @property
-    def version(self):
-        key = 'VERSION_' + self.name
-        return environ.get(key, self._version)
-
-    @property
-    def url(self):
-        key = 'URL_' + self.name
-        return environ.get(key, self._url)
 
     @property
     def versioned_url(self):
@@ -442,7 +421,7 @@ class Recipe(with_metaclass(RecipeMeta)):
                         sh.unzip(extraction_filename)
                         import zipfile
                         fileh = zipfile.ZipFile(extraction_filename, 'r')
-                        root_directory = fileh.filelist[0].filename.strip('/')
+                        root_directory = fileh.filelist[0].filename.split('/')[0]
                         if root_directory != directory_name:
                             shprint(sh.mv, root_directory, directory_name)
                     else:
