b5d4ee142e5c2b545f02378e7eeb4adef55a66cb
neymarjohn <seriousman1994217winter@gmail.com>
2016-01-15 18:03:00 +0100

    SDL2: implement support for loadingscreen

--- pythonforandroid/logger.py
+++ pythonforandroid/logger.py
@@ -7,13 +7,11 @@ from sys import stdout, stderr
 from math import log10
 from collections import defaultdict
 from colorama import Style as Colo_Style, Fore as Colo_Fore
-import six
 
 # This codecs change fixes a bug with log output, but crashes under python3
-if not six.PY3:
-    import codecs
-    stdout = codecs.getwriter('utf8')(stdout)
-    stderr = codecs.getwriter('utf8')(stderr)
+# import codecs
+# stdout = codecs.getwriter('utf8')(stdout)
+# stderr = codecs.getwriter('utf8')(stderr)
 
 # monkey patch to show full output
 sh.ErrorReturnCode.truncate_cap = 999999
