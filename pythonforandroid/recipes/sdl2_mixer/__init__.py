69bba2006e3d6a361797d82733346fb3e03f4357
neymarjohn <seriousman1994217winter@gmail.com>
2016-04-15 21:37:42 +0200

    update sdl2_ttf to 2.0.14

--- pythonforandroid/recipes/sdl2_mixer/__init__.py
+++ pythonforandroid/recipes/sdl2_mixer/__init__.py
@@ -2,10 +2,11 @@ from pythonforandroid.toolchain import BootstrapNDKRecipe
 
 
 class LibSDL2Mixer(BootstrapNDKRecipe):
-    version = '2.0.1'
+    version = '2.0.0'
     url = 'https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-{version}.tar.gz'
     dir_name = 'SDL2_mixer'
 
-    patches = ['toggle_modplug_mikmod_smpeg_ogg.patch']
+    patches = ['disable_modplug_mikmod_smpeg.patch']
+
 
 recipe = LibSDL2Mixer()
