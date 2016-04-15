a67e1267754691048019d8a77ce1d99590a83292
neymarjohn <seriousman1994217winter@gmail.com>
2016-04-15 22:48:41 +0200

    update ipaddress to 1.0.16

--- pythonforandroid/recipes/enum34/__init__.py
+++ pythonforandroid/recipes/enum34/__init__.py
@@ -1,10 +1,11 @@
+
 from pythonforandroid.toolchain import PythonRecipe
 
+
 class Enum34Recipe(PythonRecipe):
-    version = '1.1.3'
+    version = '1.0.4'
     url = 'https://pypi.python.org/packages/source/e/enum34/enum34-{version}.tar.gz'
-    depends = ['python2', 'setuptools']
+    depends = ['python2']
     site_packages_name = 'enum'
-    call_hostpython_via_targetpython = False
 
 recipe = Enum34Recipe()
