9065744c7f564689fb79249c1fc023e6dabba471
neymarjohn <seriousman1994217winter@gmail.com>
2016-11-26 02:29:59 +0100

    Restrict remove_presplash for sdl2 only

--- pythonforandroid/recipes/android/__init__.py
+++ pythonforandroid/recipes/android/__init__.py
@@ -13,7 +13,7 @@ class AndroidRecipe(IncludedFilesBehaviour, CythonRecipe):
 
     src_filename = 'src'
 
-    depends = [('pygame', 'sdl2', 'genericndkbuild'), ('python2', 'python3crystax')]
+    depends = [('pygame', 'sdl2', 'genericndkbuild'), ('python2', 'python3')]
 
     config_env = {}
 
