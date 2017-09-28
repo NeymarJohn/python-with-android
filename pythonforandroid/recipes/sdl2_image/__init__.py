9216bfec758a96bc5dd6f0220fe311edb5d4d861
neymarjohn <seriousman1994217winter@gmail.com>
2017-09-28 22:59:07 +0100

    Switched SDL2_image to use its master branch

--- pythonforandroid/recipes/sdl2_image/__init__.py
+++ pythonforandroid/recipes/sdl2_image/__init__.py
@@ -4,7 +4,8 @@ from pythonforandroid.patching import is_arch
 
 class LibSDL2Image(BootstrapNDKRecipe):
     version = '2.0.1'
-    url = 'https://www.libsdl.org/projects/SDL_image/release/SDL2_image-{version}.tar.gz'
+    # url = 'https://www.libsdl.org/projects/SDL_image/release/SDL2_image-{version}.tar.gz'
+    url = 'https://hg.libsdl.org/SDL_image/archive/tip.tar.gz'
     dir_name = 'SDL2_image'
 
     patches = ['toggle_jpg_png_webp.patch',
