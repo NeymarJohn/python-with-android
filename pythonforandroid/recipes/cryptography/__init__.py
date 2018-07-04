c9ed6fbe9d091f47d315e8bed41863d31015df47
neymarjohn <seriousman1994217winter@gmail.com>
2018-07-04 11:32:01 +0200

    Reduce docker image size

--- pythonforandroid/recipes/cryptography/__init__.py
+++ pythonforandroid/recipes/cryptography/__init__.py
@@ -1,4 +1,5 @@
 from pythonforandroid.recipe import CompiledComponentsPythonRecipe
+from os.path import join
 
 
 class CryptographyRecipe(CompiledComponentsPythonRecipe):
@@ -12,9 +13,14 @@ class CryptographyRecipe(CompiledComponentsPythonRecipe):
         env = super(CryptographyRecipe, self).get_recipe_env(arch)
         r = self.get_recipe('openssl', self.ctx)
         openssl_dir = r.get_build_dir(arch.arch)
+        env['PYTHON_ROOT'] = self.ctx.get_python_install_dir()
+        env['CFLAGS'] += ' -I' + env['PYTHON_ROOT'] + '/include/python2.7' + \
+                         ' -I' + join(openssl_dir, 'include')
         # Set linker to use the correct gcc
         env['LDSHARED'] = env['CC'] + ' -pthread -shared -Wl,-O1 -Wl,-Bsymbolic-functions'
-        env['LDFLAGS'] += ' -L' + openssl_dir + \
+        env['LDFLAGS'] += ' -L' + env['PYTHON_ROOT'] + '/lib' + \
+                          ' -L' + openssl_dir + \
+                          ' -lpython2.7' + \
                           ' -lssl' + r.version + \
                           ' -lcrypto' + r.version
         return env
