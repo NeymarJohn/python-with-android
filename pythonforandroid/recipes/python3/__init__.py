54f1556ae89789374e6a556cd98e7299ef18cecb
neymarjohn <seriousman1994217winter@gmail.com>
2019-01-30 10:30:25 +0100

    Use enaml {version} rather than master, fixes #1409

--- pythonforandroid/recipes/python3/__init__.py
+++ pythonforandroid/recipes/python3/__init__.py
@@ -21,8 +21,6 @@ class Python3Recipe(GuestPythonRecipe):
     url = 'https://www.python.org/ftp/python/{version}/Python-{version}.tgz'
     name = 'python3'
 
-    patches = ["patches/fix-ctypes-util-find-library.patch"]
-
     depends = ['hostpython3']
     conflicts = ['python3crystax', 'python2', 'python2legacy']
 
