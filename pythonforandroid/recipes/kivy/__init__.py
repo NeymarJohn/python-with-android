42ff14c5585428eb350845fe263fe795fd5cc028
neymarjohn <seriousman1994217winter@gmail.com>
2019-03-25 15:20:28 +0100

    Properly search native lib dir in ctypes, fixes #1770

--- pythonforandroid/recipes/kivy/__init__.py
+++ pythonforandroid/recipes/kivy/__init__.py
@@ -6,8 +6,8 @@ import glob
 
 
 class KivyRecipe(CythonRecipe):
-    # post kivy==1.10.1, SDL2 bugs are fixed (like orientation bug)
-    version = '44a8a6f'
+    # post kivy==1.10.1, `fixes SDL2 image loading (jpg)`
+    version = 'c4d6894'
     url = 'https://github.com/kivy/kivy/archive/{version}.zip'
     name = 'kivy'
 
