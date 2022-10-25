1982fe3005cda8991395d2bb5b534813e01a4605
neymarjohn <seriousman1994217winter@gmail.com>
2022-10-25 11:44:04 -1000

    WRITE_EXTERNAL_STORAGE maxSdk

--- pythonforandroid/recipes/aiohttp/__init__.py
+++ pythonforandroid/recipes/aiohttp/__init__.py
@@ -1,20 +1,16 @@
 """Build AIOHTTP"""
 from typing import List
-from pythonforandroid.recipe import CppCompiledComponentsPythonRecipe
+from pythonforandroid.recipe import CythonRecipe  # type: ignore
 
 
-class AIOHTTPRecipe(CppCompiledComponentsPythonRecipe):  # type: ignore # pylint: disable=R0903
-    version = "3.8.3"
-    url = "https://pypi.python.org/packages/source/a/aiohttp/aiohttp-{version}.tar.gz"
+class AIOHTTPRecipe(CythonRecipe):  # type: ignore # pylint: disable=R0903
+    """Build AIOHTTP"""
+
+    version = "v3.6.2"
+    url = "https://github.com/aio-libs/aiohttp/archive/{version}.zip"
     name = "aiohttp"
-    depends: List[str] = ["setuptools"]
-    call_hostpython_via_targetpython = False
-    install_in_hostpython = True
 
-    def get_recipe_env(self, arch):
-        env = super().get_recipe_env(arch)
-        env['LDFLAGS'] += ' -lc++_shared'
-        return env
+    depends: List[str] = ["setuptools"]
 
 
 recipe = AIOHTTPRecipe()
